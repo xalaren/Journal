a20a40283ae217ddd8426a65dc69a92a125ee89c
eterrie <imanov.03.valera@gmail.com>
2024-11-01 17:17:40 +0500

    Added Api project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,38 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        port: 4002,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:4001',
+            }
+        },
+        open: true,
+    },
+    build: {
+        minify: false,
+        cssMinify: false,
+        emptyOutDir: true,
+        outDir: '../LevelsCompletionTracker.WebApi/wwwroot',
+
+        rollupOptions: {
+            output: {
+                chunkFileNames: "assets/ts/[name].ts",
+                entryFileNames: "assets/js/[name].js",
+
+                assetFileNames: ({ name }) => {
+                    if (/\.css$/.test(name ?? "")) {
+                        return 'assets/css/[name][extname]';
+                    }
+
+                    if (/\.woff\d*$/.test(name ?? "")) {
+                        return 'assets/fonts/[name].[ext]';
+                    }
+                    return "assets/[name].[ext]";
+                },
+            },
+        }
+    },
 })
